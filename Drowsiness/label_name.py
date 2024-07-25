df2cc48fe65f1526efc5a2ee329b910ee61ef777
ZengxueYu <Z.Yu24@newcastle.ac.uk>
2024-07-25 13:24:18 +0100

    init

---
+++ Drowsiness/label_name.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2022/8/13 9:30
+@Auth ： 思绪无限
+Bilibili：思绪亦无限
+公众号：AI技术研究与分享
+@File ：label_name.py
+@IDE ：PyCharm
+功能：
+"""
+Chinese_name = {'Eyeclosed': "闭眼", 'Neutral': "正常", 'Yawn': "打哈欠"}
